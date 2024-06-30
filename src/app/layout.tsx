1ee73d86425c4c044dc242e63e025804a1aa1001
tushar-agarwal7 <tushar.agarwal7373@gmail.com>
2024-06-30 23:58:08 +0530

    make uploading page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,8 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "@/components/Navbar";
 import { Footer } from "@/components/Footer";
-
+import { Toaster } from "@/components/ui/toaster";
+import { Recursive } from 'next/font/google'
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -11,18 +12,27 @@ export const metadata: Metadata = {
   description: "Online Shop",
 };
 
-export default function RootLayout({ 
+const recursive = Recursive({ subsets: ['latin'] })
+
+export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <Navbar/>
-        {children}
-        <Footer/>
-        </body>
+    <html lang='en'>
+      <body className={recursive.className}>
+        <Navbar />
+
+        <main className='flex grainy-light flex-col min-h-[calc(100vh-3.5rem-1px)]'>
+          <div className='flex-1 flex flex-col h-full'>
+            {children}
+          </div>
+          <Footer />
+        </main>
+
+        <Toaster />
+      </body>
     </html>
-  );
+  )
 }
